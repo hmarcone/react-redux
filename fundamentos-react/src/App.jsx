1eea66016e173397eab598ca542015ebc3638663
hmarcone <hmarcone@gmail.com>
2020-11-11 14:37:21 -0300

    Subindo o fundamentos
    subindo a pasta fundamentos do react

---
+++ fundamentos-react/src/App.jsx
@@ -0,0 +1,45 @@
+import "./App.css";
+import React from "react";
+
+import Primeiro from './components/basicos/Primeiro'
+import ComParametro from './components/basicos/ComParametro'
+import Fragmento from './components/basicos/Fragmento'
+
+import Card from "./components/layout/Card";
+
+import Mega from "./components/layout/mega/Mega";
+import Aleatorio from "./components/basicos/Aleatorio";
+import Familia from "./components/basicos/Familia";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default () => (
+    <div className="App">
+        <h1>Fundamentos React</h1>
+
+        <div className="Cards">
+            <Card titulo="#05 - Componente com Filhos" color="#00C8F8">
+                <Familia sobrenome="Ferreira" />
+            </Card>
+
+            <Card titulo="#04 - Desafio Aleatório" color="#FA6900">
+                <Aleatorio min={1} max={60} />
+            </Card>
+
+            <Card titulo="#03 - Fragmento" color="#E94C6F">
+                <Fragmento />
+            </Card>
+
+            <Card titulo="#02 - Com Parâmetro" color="#E8B71A">
+                <ComParametro
+                    titulo="Situação do Aluno"
+                    aluno="Pedro Silva"
+                    nota={9.3}
+                />
+            </Card>
+
+            <Card titulo="#01 - Primeiro Componente" color="#588C73">
+                <Primeiro></Primeiro>
+            </Card>
+        </div>
+    </div>
+);

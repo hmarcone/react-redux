6882a1fa54e9be84472d0a2284949615e3266c80
hmarcone <hmarcone@gmail.com>
2020-11-16 18:38:32 -0300

    08 - Renderização Condicional

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -13,6 +13,8 @@ import Familia from "./components/basicos/Familia";
 import FamiliaMembro from "./components/basicos/FamiliaMembro";
 import ListaAlunos from "./components/repeticao/ListaAlunos";
 import TabelaProdutos from "./components/repeticao/TabelaProdutos";
+import ParOuImpar from "./components/condicional/ParOuImpar";
+import UsuarioInfo from "./components/condicional/UsuarioInfo";
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default () => (
@@ -21,6 +23,11 @@ export default () => (
 
         <div className="Cards">
 
+            <Card titulo="#08 - Renderização Condicional" color="#982395">
+                <ParOuImpar numero={20}></ParOuImpar>
+                <UsuarioInfo usuario={{ nome: "Fernando" }} />
+            </Card>
+            
             <Card titulo="#07 - Desafio Repetição" color="#3A9AD9">
                 <TabelaProdutos></TabelaProdutos>
             </Card>

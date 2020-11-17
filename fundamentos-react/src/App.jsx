2a612b837f6433fda2845a42aeb834ef2f972c05
hmarcone <hmarcone@gmail.com>
2020-11-17 18:50:23 -0300

    Componente Controlado (Input)

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -17,6 +17,7 @@ import ParOuImpar from "./components/condicional/ParOuImpar";
 import UsuarioInfo from "./components/condicional/UsuarioInfo";
 import DiretaPai from "./components/comunicacao/DiretaPai";
 import IndiretaPai from "./components/comunicacao/IndiretaPai";
+import Input from "./components/formulario/Input";
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default () => (
@@ -24,6 +25,10 @@ export default () => (
         <h1>Fundamentos React</h1>
 
         <div className="Cards">
+
+            <Card titulo="#11 - Componente Controlado (Input)" color="#E45F56">
+                <Input></Input>
+            </Card>
 
             <Card titulo="#10 - Comunicação Indireta" color="#8BAD39">
                 <IndiretaPai></IndiretaPai>

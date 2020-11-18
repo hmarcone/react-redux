8120c22b0b8279c096ed94e67a089b548c237b74
hmarcone <hmarcone@gmail.com>
2020-11-18 18:38:35 -0300

    #12 - Contador

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -18,6 +18,7 @@ import UsuarioInfo from "./components/condicional/UsuarioInfo";
 import DiretaPai from "./components/comunicacao/DiretaPai";
 import IndiretaPai from "./components/comunicacao/IndiretaPai";
 import Input from "./components/formulario/Input";
+import Contador from "./components/contador/Contador";
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default () => (
@@ -25,6 +26,14 @@ export default () => (
         <h1>Fundamentos React</h1>
 
         <div className="Cards">
+
+            <Card titulo="#13 - Mega" color="#B9006E">
+                <Mega qtde={8}></Mega>
+            </Card>            
+            
+            <Card titulo="#12 - Contador" color="#424242">
+                <Contador numeroInicial={10}></Contador>
+            </Card>
 
             <Card titulo="#11 - Componente Controlado (Input)" color="#E45F56">
                 <Input></Input>

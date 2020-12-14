378682636695ef0c9764d356a3907e38f648a6b9
Humberto Marcone <humberto.freitas@antt.gov.br>
2020-12-14 18:42:22 -0300

    Implementando as rotas

--- todo-app/frontend/src/main/app.jsx
+++ todo-app/frontend/src/main/app.jsx
@@ -4,15 +4,11 @@ import 'modules/font-awesome/css/font-awesome.min.css'
 
 import React from 'react'
 import Menu from '../template/menu'
-// import Routes from './routes'
-import Todo from '../todo/todo'
-import About from '../about/about'
-
+import Routes from './routes'
 
 export default props => (
     <div className='container'>
         <Menu />
-        <Todo />
-        <About />
+        <Routes />
     </div>
 )

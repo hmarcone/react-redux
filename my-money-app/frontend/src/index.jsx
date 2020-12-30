2af7a84a089d6fa19b2aee5aa0bb1f9ff4a3ec25
Humberto Marcone <humberto.freitas@antt.gov.br>
2020-12-30 18:59:51 -0300

    Subindo o frontend

---
+++ my-money-app/frontend/src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './man/app'
+
+ReactDOM.render(<App />
+    // <Provider store={store}>
+    //     <Routes />
+    // </Provider>
+, document.getElementById('app'))

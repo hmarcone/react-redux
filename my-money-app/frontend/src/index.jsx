8a75a778bf3fa838e9521d10a0ecf3a3037927e0
Humberto Marcone <humberto.freitas@antt.gov.br>
2021-01-06 12:26:31 -0300

    Configurando Redux

--- my-money-app/frontend/src/index.jsx
+++ my-money-app/frontend/src/index.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './man/app'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 
-ReactDOM.render(<App />
-    // <Provider store={store}>
-    //     <Routes />
-    // </Provider>
+import reducers from './main/reducers'
+
+const store = createStore(reducers)
+
+ReactDOM.render(    
+    <Provider store={store}>
+        <App />
+    </Provider>
 , document.getElementById('app'))

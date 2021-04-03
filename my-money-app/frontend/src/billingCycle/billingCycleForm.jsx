9f38301ec8d94f2feeb9bdc7fad0eae054fe2067
Humberto Marcone <hmarcone@gmail.com>
2021-04-03 20:21:46 -0300

    Adicionando o summary

--- my-money-app/frontend/src/billingCycle/billingCycleForm.jsx
+++ my-money-app/frontend/src/billingCycle/billingCycleForm.jsx
@@ -10,29 +10,28 @@ import Summary from './summary'
 
 class BillingCycleForm extends Component {
 
-    // calculateSummary() {
-    //     const sum = (t, v) => t + v
-    //     return {
-    //         sumOfCredits: this.props.credits.map(c => +c.value || 0).reduce(sum),
-    //         sumOfDebts: this.props.debts.map(d => +d.value || 0).reduce(sum)
-    //     }
-    // }
+    calculateSummary() {
+        const sum = (t, v) => t + v
+        return {
+            sumOfCredits: this.props.credits.map(c => +c.value || 0).reduce(sum),
+            sumOfDebts: this.props.debts.map(d => +d.value || 0).reduce(sum)
+        }
+    }
 
     render() {
         const { handleSubmit, readOnly, credits, debts } = this.props
-        //const { sumOfCredits, sumOfDebts } = this.calculateSummary()
+        const { sumOfCredits, sumOfDebts } = this.calculateSummary()
         return (
             <form role='form' onSubmit={handleSubmit}>
                 <div className='box-body'>
                     <Field name='name' component={LabelAndInput} readOnly={readOnly} label='Nome' cols='12 4' placeholder='Informe o nome' />
                     <Field name='month' component={LabelAndInput} type='number' readOnly={readOnly} label='Mês' cols='12 4' placeholder='Informe o mês' />
                     <Field name='year' component={LabelAndInput} type='number' readOnly={readOnly} label='Ano' cols='12 4' placeholder='Informe o ano' />
-                    {/* <Summary credit={sumOfCredits} debt={sumOfDebts} /> */}
+                    <Summary credit={sumOfCredits} debt={sumOfDebts} />
                     <ItemList cols='12 6' list={credits} readOnly={readOnly} field='credits' legend='Créditos' />
                     <ItemList cols='12 6' list={debts} readOnly={readOnly} field='debts' legend='Débitos' showStatus={true} />
                 </div>
                 <div className='box-footer'>
-                    {/* <button type='submit' className='btn btn-primary'>Submit</button> */}
                     <button type='submit' className={`btn btn-${this.props.submitClass}`}>{this.props.submitLabel}</button>
                     <button type='button' className='btn btn-default' onClick={this.props.init}>Cancelar</button> 
                 </div>
